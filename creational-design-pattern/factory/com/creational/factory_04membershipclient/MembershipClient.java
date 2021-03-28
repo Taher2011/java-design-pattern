69b8bbc1bb459a32b621e85fee6dba1efbd2051b
ADPROD\tali <taher_ali_tp@bmc.com>
2021-03-28 06:44:58 +0530

    refacoreds

--- creational-design-pattern/factory/com/creational/factory_04membershipclient/MembershipClient.java
+++ creational-design-pattern/factory/com/creational/factory_04membershipclient/MembershipClient.java
@@ -1,15 +1,14 @@
-package com.creational.factory_05membershipclient;
+package com.creational.factory_04membershipclient;
 
 import com.creational.factory_01membershipentity.Member;
-import com.creational.factory_02membershipadmin.MembershipManager;
-import com.creational.factory_04membershipenums.Membership;
+import com.creational.factory_02memberfactroy.MemberFactory;
+import com.creational.factory_03membershipenums.Membership;
 
 public class MembershipClient {
 
 	public static void main(String[] args) {
 
-		MembershipManager manager = new MembershipManager();
-		Member member = manager.createMember(Membership.LIFETIME);
+		Member member = MemberFactory.createMember(Membership.LIFETIME);
 		if (member != null) {
 			member.register();
 			member.notified();
